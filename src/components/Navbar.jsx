1a986a357a205a4424c56d0aeeafcf270deb88ca
Taisei <taisei96.web@gmail.com>
2023-11-19 07:44:22 +0900

    商品情報を定義

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux"
+import { CartIcon } from "../HeroIcons"
 
 const Navbar = () => {
 
@@ -9,7 +10,7 @@ const Navbar = () => {
       <div className="nav-center">
         <h3>Redux Shopping</h3>
         <div className="nav-container">
-          ロゴ
+          <CartIcon />
           <div className="amount-container">
             <p className="total-amount">{amount}</p>
           </div>
--- src/features/cart/CartSlice.js
+++ src/features/cart/CartSlice.js
@@ -1,8 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
+import cartItems from "../../cartItems";
 
 // 買い物かごの初期化
 const initialState = {
-  cartItems: [],
+  cartItems: cartItems,
   amount: 4,
   total: 0,
 }
