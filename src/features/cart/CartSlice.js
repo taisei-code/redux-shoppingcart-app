4463093885322b8a084573aa11738395b0a70055
Taisei <taisei96.web@gmail.com>
2023-11-19 07:08:15 +0900

    storeにアクセス

--- src/features/cart/CartSlice.js
+++ src/features/cart/CartSlice.js
@@ -3,12 +3,14 @@ import { createSlice } from "@reduxjs/toolkit";
 // 買い物かごの初期化
 const initialState = {
   cartItems: [],
-  amount: 0,
+  amount: 4,
   total: 0,
 }
 
 const cartSlice = createSlice({
-  name "cart",
+  name: "cart",
   initialState,
   reducers: {},
 })
+
+export default cartSlice.reducer;
