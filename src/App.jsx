e7b95ec43a3fa9aab27ab74de0ddcdf39cf3545a
Taisei <taisei96.web@gmail.com>
2023-11-19 09:33:59 +0900

    購入する商品を昇降

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import CartContainer from './components/CartContainer'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <main>
         <Navbar />
+        <CartContainer />
       </main>
     </>
   )
--- src/features/cart/CartSlice.js
+++ src/features/cart/CartSlice.js
@@ -11,7 +11,26 @@ const initialState = {
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    clearCart: (state) => {
+      // state.cartItems = [];
+      return { cartItems: [], amount: 0, total: 0 }
+    },
+    removeItem: (state, action) => {
+      // console.log(action)
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId)
+    },
+    increase: (state, action) => {
+      const cartItem = state.cartItems.find((item) => item.id === action.payload)
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, action) => {
+      const cartItem = state.cartItems.find((item) => item.id === action.payload)
+      cartItem.amount = cartItem.amount - 1;
+    }
+  },
 })
 
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions
 export default cartSlice.reducer;
